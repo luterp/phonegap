439fff2361203ea8f5c0ab40ec2e8682a7c8fc05
luterp <luter.paul@gmail.com>
2014-12-01 16:56:36 +1100

    Initial

---
+++ js/apiCalls.js
@@ -0,0 +1,36 @@
+ 
+//add code here to handle camera
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+	console.log(navigator.camera);
+	alert('Device is ready');
+	// set up a variable...
+}
+
+function TakePhotoUsingCamera() {
+	TakePhoto(Camera.PictureSourceType.CAMERA)
+}
+
+function TakePhotoFromLibrary() {
+	TakePhoto(Camera.PictureSourceType.PHOTOLIBRARY);
+}
+
+function onSuccess(imageData) {
+	var image = document.getElementById('myImage');
+	image.src = "data:image/jpeg;base64," + imageData;  
+}
+
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
+
+function TakePhoto(sourceType) {
+var camOptions = {
+quality:50,
+destinationType: Camera.DestinationType.DATA_URL,
+sourceType: sourceType,
+correctOrientation: true	
+};
+navigator.camera.getPicture(onSuccess, onFail, camOptions);
+}
+
